7cfdf92fba8ab64ccaf4d0ee68cd27e4b2c1eafb
sengarjanhavi <janhavisengar@gmail.com>
2024-09-10 00:18:26 +0200

    Update index.ts

--- frontend-service/src/index.ts
+++ frontend-service/src/index.ts
@@ -1,7 +1,5 @@
 import { Application } from 'express';
 import createApp from './app';
-import { TradeConsumerInstance } from './components/positions/entry-points/message-queue';
-
 const PORT: number = parseInt(process.env.PORT || '3001', 10);
 const server : Application = createApp();
 
