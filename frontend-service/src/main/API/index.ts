360c7535bdf98fbeaefbe2389066bee78a2daf5d
sengarjanhavi <janhavisengar@gmail.com>
2024-09-10 00:12:03 +0200

    Create index.ts

---
+++ frontend-service/src/main/API/index.ts
@@ -0,0 +1,51 @@
+import { Router, Request, Response } from 'express';
+import { addClient, removeClient, notifyClients } from '../../main/events-manager';
+import { ServerResponseDTO } from '../../main/responseDTO';
+import { OpenPositionDTO } from '../../main/positionDTO';
+
+const ROUTER: Router = Router();
+
+ROUTER.get('/', (req: Request, res: Response) => {
+    try {
+        // Establish SSE connection
+        const headers = {
+            'Content-Type': 'text/event-stream',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache'
+        };
+
+        res.writeHead(200, headers);
+
+        const client = addClient(res);
+
+        const openPosition: number = TradeConsumerInstance.getOpenPosition();
+        const response: ServerResponseDTO<OpenPositionDTO> = {
+            success: true,
+            data: { openPosition },
+        };
+        client.sendEvent(response);
+
+        req.on('close', () => {
+            removeClient(client);
+        });
+    } catch (error: unknown) {
+        let errorMessage: string;
+
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        } else if (typeof error === 'string') {
+            errorMessage = error;
+        } else {
+            errorMessage = 'An unknown error occurred';
+        }
+
+        const errorResponse: ServerResponseDTO<OpenPositionDTO> = {
+            success: false,
+            message: errorMessage,
+        };
+
+        res.status(500).json(errorResponse);
+    }
+});
+
+export default ROUTER;
