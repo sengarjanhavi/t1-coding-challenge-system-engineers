d02449ac7a5b254d2f91049cf8c38c5de3980b1e
sengarjanhavi <janhavisengar@gmail.com>
2024-09-10 00:02:27 +0200

    Update index.ts

--- frontend-service/src/index.ts
+++ frontend-service/src/index.ts
@@ -1,1 +1,11 @@
-// YOUR CODE HERE
+import { Application } from 'express';
+import createApp from './app';
+import { TradeConsumerInstance } from './components/positions/entry-points/message-queue';
+
+const PORT: number = parseInt(process.env.PORT || '3001', 10);
+const server : Application = createApp();
+
+server.listen(PORT, () => {
+    TradeConsumerInstance.start();
+    console.log(`Server is running on port ${PORT}`);
+})
